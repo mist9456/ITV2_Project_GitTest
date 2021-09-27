17c590e2dae5120187250de1ed77cd341feefd23
BlackfiremasterX <misharozental@gmail.com>
2021-09-27 15:31:36 +0700

    Update 3

--- src/main.kt
+++ src/main.kt
@@ -1,1 +1,19 @@
-ghnchvbnv sdrftghnb
+fun main() {
+    Task0()
+    Task1()
+    Task2()
+}
+
+fun Task0()
+{
+    print("Hello World!")
+}
+fun Task1()
+{
+    print("Proof your proffesionality!")
+}
+fun Task2()
+{
+
+    print("All is OK!")
+}
