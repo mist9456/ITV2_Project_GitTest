d0408f4d901d02e001959868d80c6ecd2f2cf3b5
BlackfiremasterX <misharozental@gmail.com>
2021-10-04 14:36:29 +0700

    Update 5

--- src/main.kt
+++ src/main.kt
@@ -1,25 +1,131 @@
+import java.util.*
+import kotlin.math.sqrt
+
 fun main() {
-    Task0()
-    Task1()
-    Task2()
-    Task3()
+
+    Info()
+    var user_answer = ""
+    var run = true
+
+    loop@ while (true) {
+        print(
+            "\n\nВыберите задачу из списка!\n" +
+                    "Вводите Ваш запрос:"
+        )
+        user_answer = readLine().toString().toLowerCase()
+        when (user_answer) {
+            "b1" -> Begin1()
+            "b4" -> Begin4()
+            "b7" -> Begin7()
+            "b9" -> Begin9()
+            "if5" -> if5()
+            "if6" -> if6()
+            "if7" -> if7()
+            "if8" -> if8()
+            else -> print("Такой задачи в списке нет!")
+        }
+        run = true
+        while (run) {
+            print("\n\nПродолжить?[y/n]")
+            user_answer = readLine().toString().toLowerCase()
+            when (user_answer) {
+                "y" -> {
+                    print("Продолжаем")
+                    for (i in 0..2) {
+                        Thread.sleep(333)
+                        print(".")
+                    }
+                    run = false
+                    continue@loop
+                }
+                "n" -> {
+                    print("До свидания")
+                    for (i in 0..2) {
+                        Thread.sleep(333)
+                        print("!")
+                    }
+                    run = false
+                    break@loop
+                }
+                else -> print("Тут всего 2 варианта ответа[y/n]")
+            }
+        }
+    }
 }
 
-fun Task0()
-{
-    print("Hello World!")
-}
-fun Task1()
-{
-    print("Proof your proffesionality!")
-}
-fun Task2()
-{
 
-    print("All is OK!")
-}
-fun Task3()
-{
+fun Info() {
 
-    print("All is not OK!")
+    var timing = 200
+    print("Бобро пожаловать в консольную версию задачника с примерами!\n")
+    Thread.sleep(timing.toLong())
+    print("y - кнопка отвечающая за ответ \"да\"\n")
+    Thread.sleep(timing.toLong())
+    print("n - кнопка отвечающая за ответ \"нет\"\n")
+    Thread.sleep(timing.toLong())
+    print("Код задачи определяется ее первой буквой в названии\n")
+    Thread.sleep(timing.toLong())
+    print("+ порядковый номер в списке.\n")
+    Thread.sleep(timing.toLong())
+    print("Например, Begin1 - b1.\n")
+    Thread.sleep(timing.toLong())
+    print("После ввода ответа нужно нажать клавишу Enter!")
+    Thread.sleep(timing.toLong())
+}
+
+fun Begin1() {
+
+    println("\n\nBegin1. Дана сторона квадрата a. Найти его периметр P = 4*a")
+    print("Введите сторону квадрата a: ")
+    var a = readLine()!!.toDouble()
+    var P = 4 * a
+    print("Периметр квадрата со стороной а = $a: $P")
+
+}
+
+
+fun Begin4() {
+
+    println(
+        "\n\nBegin4. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве\n" +
+                "значения π использовать 3.14."
+    )
+    print("Введите диамерт окружности d: ")
+    var d = readLine()!!.toDouble()
+    var L = 3.14 * d
+    print("диаметр окружности d с длинной L = $d: $L")
+
+
+}
+
+
+fun Begin7() {
+    println(
+        "Найти длину окружности L и площадь круга S заданного радиуса R:\n" +
+                "L = 2·π·R, S = π·R^2"
+    )
+    print("Введите радиус R:")
+
+
+    val r: Double = Scanner(System.`in`).nextDouble()
+    val l = 2 * 3.14 * r
+    val s = 3.14 * r * r
+    println("L = $l")
+    println("S = $s")
+
+}
+
+fun Begin9() {
+
+    println(
+        "\n\nBegin9.  Даны два неотрицательных числа a и b. Найти их среднее геометрическое, то есть квадратный корень из их произведения: √\n" +
+                "a·b."
+    )
+    print("Введите a: ")
+    print("Введите b: ")
+    var a = 3
+    var b = 9.0
+    var SQRT = sqrt(b)
+    println("Корень квадратный из $b = $SQRT")
+
 }
